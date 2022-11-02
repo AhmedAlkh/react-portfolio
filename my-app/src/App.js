ed81e6d101ccfd9ad28d7327c9389fb0c0f12385
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-11-02 11:36:58 -0400

    created footer component and added copyright text, included some styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
         <About />
         <Projects />
         <Contact />
+        <Footer />
         </div>
     )
 }
---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { CFooter } from '@coreui/react'
+
+function Footer() {
+    return (
+        <CFooter>
+  <div>
+    <h3 className="footerText">© 2022 All Right Reserved.</h3>
+  </div>
+</CFooter>
+    )
+}
+
+export default Footer;
