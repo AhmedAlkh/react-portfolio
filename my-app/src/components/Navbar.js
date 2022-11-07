3472ad5df0c27f850c220aef4e7a0db88b21bd8a
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-11-07 08:49:07 -0500

    removed drop down from navbar, added my 4 link buttons, updated readme

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -3,30 +3,20 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function NavBar() {
     return (
         <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Ahmed's Portfolio</Navbar.Brand>
+          <Navbar.Brand href="#home" className="navTitle">Ahmed's Portfolio</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav ml-auto">
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link href="#">About me</Nav.Link>
+              <Nav.Link href="#">Projects</Nav.Link>
+              <Nav.Link href="#">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
