d77e30d56b8e6f57855862529a41fda71d86fd62
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-11-08 12:20:26 -0500

    updated code in navbar to use props

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -5,19 +5,32 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 
-function NavBar() {
+function NavBar(props) {
+  const { currentSection, setCurrentSection } = props;
+
     return (
-        // Remove light class to enable css styling for background color
         <Navbar expand="lg"> 
         <Container>
           <Navbar.Brand href="#about">Ahmed's Portfolio</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#about">About me</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link className={currentSection === 'about'}>
+              <span onClick={() => setCurrentSection('about')}>About Me</span>
+            </Nav.Link>
+            <Nav.Link className={currentSection === 'projects'}>
+              <span onClick={() => setCurrentSection('projects')}>Projects</span>
+            </Nav.Link>
+            <Nav.Link className={currentSection === 'contact'}>
+              <span onClick={() => setCurrentSection('contact')}>Contact</span>
+            </Nav.Link>
+            <Nav.Link className={currentSection === 'resume'}>
+              <span onClick={() => setCurrentSection('resume')}>Resume</span>
+            </Nav.Link>
+              {/* <Nav.Link href="#about">About me</Nav.Link> */
+              /* <Nav.Link href="#projects">Projects</Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
-              <Nav.Link href="#resume">Resume</Nav.Link>
+              <Nav.Link href="#resume">Resume</Nav.Link> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
