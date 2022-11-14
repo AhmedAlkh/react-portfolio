b8d322c0bfce8b440a008083d607af274499f178
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-11-14 12:22:10 -0500

    updated resume page, needs to be styled

--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -1,5 +1,28 @@
-// import React from "react";
+import React from "react";
 
-function Resume() {}
+function Resume() {
+    return (
+		<section>
+			<div className="center">
+				<h1>Resume</h1>
+			</div>
+			<div>
+				<a className="center" href={require("../assets/resume/resume2022.pdf")} target="_blank" rel="noreferrer">
+					<h4>Download My Resume</h4>
+				</a>
+			</div>
+            <div>
+                <h3 className="skills">Skills</h3>
+                <hr />
+                    <ul>
+                        <li>Front-End: HTML/CSS/JavaScript/Bootstrap/React</li>
+                        <li>Back-End: APIs/Node/Express/MVC/PWA</li>
+                        <li>Dev Tools: Github/NPM/Webpack</li>
+                        <li>Database: MySQL/NoSQL/MongoDB/GraphQL</li>
+                    </ul>
+            </div>
+		</section>
+	);
+}
 
 export default Resume;
