442a3640b3c4f2ed2e02549fbe2e18b08203b04b
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-11-14 13:10:04 -0500

    resume page is complete

--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -1,28 +1,48 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 function Resume() {
-    return (
-		<section>
-			<div className="center">
-				<h1>Resume</h1>
-			</div>
-			<div>
-				<a className="center" href={require("../assets/resume/resume2022.pdf")} target="_blank" rel="noreferrer">
-					<h4>Download My Resume</h4>
-				</a>
-			</div>
-            <div>
-                <h3 className="skills">Skills</h3>
-                <hr />
-                    <ul>
-                        <li>Front-End: HTML/CSS/JavaScript/Bootstrap/React</li>
-                        <li>Back-End: APIs/Node/Express/MVC/PWA</li>
-                        <li>Dev Tools: Github/NPM/Webpack</li>
-                        <li>Database: MySQL/NoSQL/MongoDB/GraphQL</li>
-                    </ul>
-            </div>
-		</section>
-	);
+  return (
+    <section className="resume-page">
+      <div className="center">
+        <h1>Resume</h1>
+      </div>
+      <div className="btn-div">
+        <Button
+          className="resume-btn"
+          href={require("../assets/resume/resume2022.pdf")}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h4>Download My Resume</h4>
+        </Button>
+      </div>
+      <div className="resume-screenshots">
+        <img
+        className="resume"
+          src={require("../assets/screenshots/resume1.png")}
+          alt="Resume screenshot"
+        />
+        <img
+        className="resume"
+          src={require("../assets/screenshots/resume2.png")}
+          alt="Resume screenshot"
+        />
+      </div>
+      <div className="centering-cont">
+      <div className="skills-container">
+        <h3 className="skills-title">Skills</h3>
+        <hr></hr>
+        <ul>
+          <li>Front-End: HTML/CSS/JavaScript/Bootstrap/React</li>
+          <li>Back-End: APIs/Node/Express/MVC/PWA</li>
+          <li>Dev Tools: Github/NPM/Webpack</li>
+          <li>Database: MySQL/NoSQL/MongoDB/GraphQL</li>
+        </ul>
+      </div>
+      </div>
+    </section>
+  );
 }
 
 export default Resume;
