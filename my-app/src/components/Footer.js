850a32b97d7fc036284b0e2b1437d0a0eddd4501
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-11-20 21:24:08 -0500

    added social icons to footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
 
 function Footer() {
     return (
   <div className="footerDiv">
+    <FaGithub className="footer-icon" />
+    <FaLinkedin className="footer-icon" />
+    <FaEnvelope className="footer-icon" />
     <h5 className="footerText">© 2022 All Rights Reserved.</h5>
   </div>
     )
