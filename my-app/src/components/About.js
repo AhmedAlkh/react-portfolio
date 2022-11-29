b0e5b0315ba650565f5ef8875d4b43c219a7ce16
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-11-29 11:36:34 -0500

    updated about me text

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -17,9 +17,10 @@ function About() {
       <p>
           Welcome to my portfolio! My name is Ahmed Alkhatib and the bulk of my work experience has been
           in construction and warehousing. I've always had an interest in technology and in 2021 I decided
-          to enroll in a coding program offered by University of Toronto. The program I enrolled in focused
-          on website and mobile app development, teaching me to become a full-stack developer! I look 
-          forward to learning more and exploring new opportunities. In my personal time I enjoy working out in the gym,
+          to enroll in a coding program offered by University of Toronto to become a full-stack developer. I was introduced to the basic concepts and technologies of
+          mobile app and web development. Throughout the duration of the course I had the opportunity to build several independant projects
+          as well as some team projects in collaboration with class mates.  I look 
+          forward to learning more and exploring new opportunities as I transition into a career as a web developer. In my personal time I enjoy working out in the gym,
           driving my car while listening to music, playing video games, and spending time with friends and family.
       </p>
       </div>
